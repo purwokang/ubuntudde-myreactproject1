29df3e9f4aa30e38344def99fb640a9fb2db56c4
purwokang <purwo.com@gmail.com>
2020-12-11 23:30:30 +0700

    semoga lancar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <p>Semoga lancar jaya!</p>
+          <h2>semoga!</h2>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
